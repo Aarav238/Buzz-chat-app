ba5835bee1d7bdaae8f29e4fa7399bd903f71bb5
Aarav Shukla <aarav8090shukla@gmail.com>
2022-11-11 11:36:15 +0530

    Add files via upload

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+// export const host = "http://localhost:5000";
+export const host = "https://chat-app-aarav.herokuapp.com";
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoutes = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
